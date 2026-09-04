3ba20bca874892ef99dfafcadeef30df246ce6ed
Madeline Hashcube <madeline@hashcube.dev>
2026-09-04 18:44:07 -0500

    Add package to Nix Flake (#21)
    * epic nix oneshot hell yes
    
    * slight fix to get the version info in the name whoops
    
    * nitpicks
    
    * update Flake lock
    
    ---------
    
    Co-authored-by: jel <git@jel.gay>

--- flake.nix
+++ flake.nix
@@ -36,6 +36,18 @@
 
           RUST_SRC_PATH = "${pkgs.fenix.complete.rust-src}/lib/rustlib/src/rust/library";
         };
+
+        packages.default =
+          let
+            manifest = pkgs.lib.importTOML ./Cargo.toml;
+          in
+          pkgs.rustPlatform.buildRustPackage {
+            pname = manifest.package.name;
+            inherit (manifest.package) version;
+            src = pkgs.lib.cleanSource ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+            meta.mainProgram = "lucida";
+          };
       }
     );
 }
